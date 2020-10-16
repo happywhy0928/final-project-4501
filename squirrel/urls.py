4aedcf4a6fa7965961facac005d51e193b882118
Hongyu Wang <hw2801@columbia.edu>
2020-10-16 22:21:25 +0000

    solve the error by index.html

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -6,5 +6,5 @@ app_name = 'squirrel'
 
 urlpatterns = [
         path('', views.index, name ='index'),
-        path('<unique-squirrel-id>/', views.detail, name='detail'),
+        path('<int:squirrel_id>/', views.detail, name='detail'),
         ]
